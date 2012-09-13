fe8d9b35631c44c2681166399272030d7cc275ad
Leif Ringstad <leifcr@gmail.com>
2012-09-13 14:21:06 +0200

    Fixes for tests for ree

--- test/unit/test_diff.rb
+++ test/unit/test_diff.rb
@@ -14,10 +14,10 @@ class TestVersioningDiff < Test::Unit::TestCase
       @post.save
     end
     should 'get html diff' do
-      @post.diff(1,2,:html).should == "<div class=\"diff\"><div class=\"single_diff\"><div class=\"key_title\">title</div><div class=\"del\">Sugar Donkey</div></div><div class=\"single_diff\"><div class=\"key_title\">title</div><div class=\"add\">Monkey Business</div></div><div class=\"single_diff\"><div class=\"key_title\">length</div><div class=\"del\">41</div></div><div class=\"single_diff\"><div class=\"key_title\">length</div><div class=\"add\">12</div></div></div>"
+      assert @post.diff(1,2,:html) == "<div class=\"diff\"><div class=\"single_diff\"><div class=\"key_title\">title</div><div class=\"del\">Sugar Donkey</div></div><div class=\"single_diff\"><div class=\"key_title\">title</div><div class=\"add\">Monkey Business</div></div><div class=\"single_diff\"><div class=\"key_title\">length</div><div class=\"del\">41</div></div><div class=\"single_diff\"><div class=\"key_title\">length</div><div class=\"add\">12</div></div></div>"
     end
     should 'get ascii diff' do
-      @post.diff(1,2,:ascii).should == "Key: title\n----------\n-Sugar Donkey\n+Monkey Business\n\nKey: length\n-----------\n-41\n+12\n\nKey: comments\n-------------\n \n\n"
+      assert @post.diff(1,2,:ascii) == "Key: title\n----------\n-Sugar Donkey\n+Monkey Business\n\nKey: length\n-----------\n-41\n+12\n\nKey: comments\n-------------\n \n\n"
     end
   end # context 'when doing diff between versioned posts' do
 end
